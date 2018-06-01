da7b0c23c9e5b338b0f8e13f8aa341e2810fe242
Tom Dykstra <tdykstra@microsoft.com>
2018-06-01 14:39:05 -0700

    rearrange some code

--- CosmosDBSamplesV2/DocsByUsingDocumentClient.cs
+++ CosmosDBSamplesV2/DocsByUsingDocumentClient.cs
@@ -8,7 +8,6 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
-using System.Web;
 
 namespace CosmosDBSamplesV2
 {
@@ -25,15 +24,17 @@ namespace CosmosDBSamplesV2
             TraceWriter log)
         {
             log.Info("C# HTTP trigger function processed a request.");
-            var searchterm = req.RequestUri.ParseQueryString().Get("searchterm");
-            Uri collectionUri = UriFactory.CreateDocumentCollectionUri("ToDoItems", "Items");
 
+            var searchterm = req.RequestUri.ParseQueryString().Get("searchterm");
             if (string.IsNullOrWhiteSpace(searchterm))
             {
                 return req.CreateResponse(HttpStatusCode.NotFound);
             }
 
+            Uri collectionUri = UriFactory.CreateDocumentCollectionUri("ToDoItems", "Items");
+
             log.Info($"Searching for word: {searchterm} using Uri: {collectionUri.ToString()}");
+
             IDocumentQuery<ToDoItem> query = client.CreateDocumentQuery<ToDoItem>(collectionUri)
                 .Where(p => p.Description.Contains(searchterm))
                 .AsDocumentQuery();
